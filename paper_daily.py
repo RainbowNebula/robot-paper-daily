5ce937e014faf34b2d3b2d6a3faaa626c0fbf899
RainbowNebula <cometq@foxmail.com>
2025-09-24 15:54:19 +0800

    Update paper_daily.py

--- paper_daily.py
+++ paper_daily.py
@@ -55,6 +55,7 @@ LLM_API_KEY = os.getenv("LLM_API_KEY")  # 从环境变量读取密钥
 LLM_API_HOST = "api.chatanywhere.tech" # https://github.com/chatanywhere/GPT_API_free
 LLM_API_ENDPOINT = "/v1/chat/completions"
 LLM_MODEL = "gpt-4o-mini"
+LLM_PROMPT = os.getenv("LLM_PROMPT")
 
 # 爬取配置
 REQUEST_INTERVAL = 1.2
@@ -182,18 +183,7 @@ def get_first_author(authors_str: str) -> str:
 
 def call_llm_for_summary(title: str, abstract: str, introduction: str) -> Dict:
     """调用LLM生成总结，并提取1-5分相关性评分"""
-    system_prompt = """你是具身智能领域研究员，主要研究3D计算机视觉、vison-language、主动感知、robot maniuplation，不关心硬件相关创新，基于论文标题、摘要和引言，按以下格式输出：
-【关注的是什么问题】
-1. 核心问题1（≤40词）
-2. 核心问题2（≤40词）
-....
-n. 核心问题n（≤40词）
-【用了什么创新的方案】
-核心解决方案（1-8句话）
-【相关性评分】
-分数：x分（x为1-5的整数，1分最低，5分最高，需基于你的研究领域进行相关性评定,评分严谨，与自己研究内容直接相关才是五分，不用给出理由）
-为了逻辑通顺保留英文关键词，不要额外内容，严格按格式输出。"""""
-
+    system_prompt = LLM_PROMPT
     user_prompt = f"标题：{title}\n摘要：{abstract}\n引言：{introduction}"
     payload = json.dumps({
         "model": LLM_MODEL,
